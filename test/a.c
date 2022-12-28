21f4927f606f9386b4d14201123feaa8d773fa09
fulara1102 <fularagaurav1102@gmail.com>
2022-12-28 23:06:56 +0530

    initial commit

---
+++ test/a.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a,b,res;
+    printf("Enter two numbers :");
+    scanf("%d%d",&a,&b);
+    res = a + b;
+    printf("sum is : %d",res);
+    return 0;
+}
